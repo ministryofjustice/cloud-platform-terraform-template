0895aae9b89cf8c13d73b44975275ea91e89ff6e
jakemulley <jakemulley@users.noreply.github.com>
2023-08-14 19:07:20 +0100

    Refactor terraform-template to add standardised README and tags

--- examples/main.tf
+++ examples/main.tf
@@ -1,7 +1,16 @@
 provider "aws" {
-  region  = "eu-west-2"
+  region = "eu-west-2"
 }
 
 module "template" {
   source = "../"
+
+  # Tags
+  business_unit          = var.business_unit
+  application            = var.application
+  is_production          = var.is_production
+  team_name              = var.team_name
+  namespace              = var.namespace
+  environment_name       = var.environment_name
+  infrastructure_support = var.infrastructure_support
 }
